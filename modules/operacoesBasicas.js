977641588610844add3ce28507eae5ed3e65d54c
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-02-01 01:56:04 -0300

    :wrench: Criando módulo de operações básicas

---
+++ modules/operacoesBasicas.js
@@ -0,0 +1,20 @@
+const operacoesBasicas = {
+    soma: (n1, n2) => {
+      return n1 + n2;
+    },
+
+    multiplicacao: (n1, n2) => {
+      return n1 * n2;
+    },
+
+    divisao: (n1, n2) => {
+      return n1 / n2;
+    },
+
+    subtracao: (n1, n2) => {
+      return n1 - n2;
+    },
+  };
+  
+  module.exports = operacoesBasicas;
+  
