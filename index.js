78803716b0d04a710f4d5b54b91c3248360c0730
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-02-03 00:35:56 -0300

    :wrench: Adicionando novos módulos ao index

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const config = require("./dist/Config/config");
 const operacoes = require("./dist/Operacoes/operacoes");
 const geometria = require("./dist/Geometria/geometria");
 const math = require("./dist/Math/math");
+const equacoes = require("./dist/Equacoes/equacoes");
+const media = require("./dist/Media/media");
 
-module.exports = { ...config, ...operacoes, ...geometria, ...math };
+module.exports = {
+  ...config,
+  ...operacoes,
+  ...geometria,
+  ...math,
+  ...equacoes,
+  ...media,
+};
